f1e48d9f94be742f3f7c11ef0d8c4e8cb92bed45
Fernandico Geovardo <fernandico.geovardo01@gmail.com>
2024-06-17 23:10:04 +0700

    fix: userId in banks

--- bank/bankCreateRequest.go
+++ bank/bankCreateRequest.go
@@ -1,7 +1,7 @@
 package bank
 
 type BankCreateRequest struct {
-	UserID int    `form:"user_id" binding:"required"`
+	UserID int    `json:"user_id" binding:"required"`
 	Type   string `json:"type" binding:"required"`
 	Name   string `json:"name" binding:"required"`
 	Number string `json:"number" binding:"required"`
